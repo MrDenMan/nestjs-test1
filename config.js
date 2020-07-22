44398766f8b9f138183b7ae8fe7ee89ae3457bfa
mrdenman <den2014is@yandex.ru>
2020-07-22 11:45:32 +0300

    Add config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Configure and rename to "config.js"
+// Create an empty file called "sentry" in same directory
+// Have steamguard code = "" to start
+// Fill in the steamguard code after receiving it
+// After successfully logging in, return steamguard code to ""
+var config = {};
+
+config.steam_name = "mrdenmanpro";
+config.steam_user = "mrdenmanpro";
+config.steam_pass = "NJ0hkw13";
+config.steam_guard_code = "";
+
+module.exports = config;
